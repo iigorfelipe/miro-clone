6858232c95908219b09094eff88fb605c697c7f1
Igor <iigorfelipe@gmail.com>
2024-06-19 12:29:55 -0300

    ✨ add organizations to the sidebar

---
+++ components/hint.tsx
@@ -0,0 +1,51 @@
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger
+} from "@/components/ui/tooltip";
+import React from "react";
+
+export interface HinProps {
+  label: string;
+  children: React.ReactNode;
+  side?: "top" | "bottom" | "left" | "right";
+  align?: "start" | "center" | "end";
+  sideOffset?: number; 
+  alignOffset?: number; 
+};
+
+const Hint = ({
+  label,
+  children,
+  side,
+  align,
+  sideOffset,
+  alignOffset,
+}: HinProps) => {
+  return (
+    <TooltipProvider>
+      <Tooltip delayDuration={100}>
+        
+        <TooltipTrigger asChild>
+          {children}
+        </TooltipTrigger>
+
+        <TooltipContent
+          className="text-white bg-black border-black"
+          side={side}
+          align={align}
+          sideOffset={sideOffset}
+          alignOffset={alignOffset}
+        >
+          <p className="font-semibold capitalize">
+            {label}
+          </p>
+        </TooltipContent>
+
+      </Tooltip>
+    </TooltipProvider>
+  );
+};
+
+export default Hint;
