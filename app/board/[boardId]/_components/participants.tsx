835760aff0fec86b90e6c96d2002bab013d428b6
Igor <iigorfelipe@gmail.com>
2024-07-17 22:32:35 -0300

    ✨ implements avatar display for participants

--- app/board/[boardId]/_components/participants.tsx
+++ app/board/[boardId]/_components/participants.tsx
@@ -1,9 +1,51 @@
+"use client";
+
+import { useOthers, useSelf } from "@/liveblocks.config";
+import { UserAvatar } from "./user-avatar";
+import { connectionIdToColor } from "@/lib/utils";
+
+const MAX_SHOW_USERS = 2;
+
 export const Participants = () => {
+  const users = useOthers();
+  const currentUser = useSelf();
+  const hasMoreUsers = users.length > MAX_SHOW_USERS;
+
   return (
-    <div
-      className="absolute h-12 top-2 right-2 bg-white rounded-md p-3 flex items-center shadow-md"
-    >
-      List of users
+    <div className="absolute h-12 top-2 right-2 bg-white rounded-md p-3 flex items-center shadow-md">
+      <div className="flex gap-x-2"> 
+        {
+          users.slice(0, MAX_SHOW_USERS).map(({ connectionId, info }) => {
+            return (
+              <UserAvatar
+                key={connectionId}
+                borderColor={connectionIdToColor(connectionId)}
+                src={info?.avatar}
+                name={info?.name}
+                fallback={info?.name?.[0] || "T"}
+              />
+            )
+          })
+        }
+        {
+          currentUser && (
+            <UserAvatar
+              borderColor={connectionIdToColor(currentUser.connectionId)}
+              src={currentUser.info?.avatar}
+              name={`${currentUser.info?.name} (You)`}
+              fallback={currentUser.info?.name?.[0]}
+            />
+          )
+        }
+        {
+          hasMoreUsers && (
+            <UserAvatar
+              src={`${users.length - MAX_SHOW_USERS} more`}
+              fallback={`+${users.length - MAX_SHOW_USERS}`}
+            />
+          )
+        }
+      </div>
     </div>
   );
 };
