755b10e782c97f7241cf5da3fa7f62356651ee22
Igor <iigorfelipe@gmail.com>
2024-07-26 23:16:20 -0300

    ✨ implements text and note layer

--- app/board/[boardId]/_components/layer-preview.tsx
+++ app/board/[boardId]/_components/layer-preview.tsx
@@ -4,7 +4,9 @@ import { useStorage } from "@/liveblocks.config";
 import { LayerType } from "@/types/canvas";
 import { memo } from "react";
 import { Ellipse } from "./ellipse";
+import { Note } from "./note";
 import { Rectangle } from "./rectangle";
+import { Text } from "./text";
 
 interface LayerPreviewProps {
   id: string;
@@ -24,6 +26,24 @@ export const LayerPreview = memo(({
   };
 
   switch (layer.type) {
+    case LayerType.Note:
+      return (
+        <Note
+          id={id}
+          layer={layer}
+          onPointerDown={onLayerPointerDown}
+          selectionColor={selectionColor}
+        />
+      )
+    case LayerType.Text:
+      return (
+        <Text
+          id={id}
+          layer={layer}
+          onPointerDown={onLayerPointerDown}
+          selectionColor={selectionColor}
+        />
+      )
     case LayerType.Ellipse:
       return (
         <Ellipse
